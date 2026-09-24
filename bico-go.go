b23b2be59233acdd05f65055175c8ffed6ff08e5
Guilherme-Costa-Dev <guilhermecosta030409@gmail.com>
2026-09-24 17:07:42 -0300

    CORS adicionado

--- bico-go.go
+++ bico-go.go
@@ -25,17 +25,19 @@ func main() {
 	prestadorHandler := handler.NewPrestadorHandler(db, authClient)
 
 	http.HandleFunc("POST /clientes", clienteHandler.CreateCliente)
-	http.HandleFunc("GET /clientes", middleware.AuthMiddleware(authClient, clienteHandler.DadosCliente))
-	http.HandleFunc("PUT /clientes", middleware.AuthMiddleware(authClient, clienteHandler.AtualizarCliente))
-	http.HandleFunc("DELETE /clientes", middleware.AuthMiddleware(authClient, clienteHandler.DeletarCliente))
+	http.HandleFunc("GET /clientes", middleware.Auth(authClient, clienteHandler.DadosCliente))
+	http.HandleFunc("PUT /clientes", middleware.Auth(authClient, clienteHandler.AtualizarCliente))
+	http.HandleFunc("DELETE /clientes", middleware.Auth(authClient, clienteHandler.DeletarCliente))
 
 	http.HandleFunc("POST /prestadores", prestadorHandler.CreatePrestador)
-	http.HandleFunc("GET /prestadores", middleware.AuthMiddleware(authClient, prestadorHandler.DadosPrestador))
-	http.HandleFunc("PUT /prestadores", middleware.AuthMiddleware(authClient, prestadorHandler.AtualizarPrestador))	
-	http.HandleFunc("DELETE /prestadores", middleware.AuthMiddleware(authClient, prestadorHandler.DeletarPrestador))
 	http.HandleFunc("GET /prestadores/lista", prestadorHandler.ListarPrestadores)
+	http.HandleFunc("GET /prestadores", middleware.Auth(authClient, prestadorHandler.DadosPrestador))
+	http.HandleFunc("PUT /prestadores", middleware.Auth(authClient, prestadorHandler.AtualizarPrestador))
+	http.HandleFunc("DELETE /prestadores", middleware.Auth(authClient, prestadorHandler.DeletarPrestador))
 
-	err = http.ListenAndServe(":8080", nil)
+	handler := middleware.Cors(http.DefaultServeMux)
+
+	err = http.ListenAndServe(":8080", handler)
 	if err != nil {
 		log.Fatal(err)
 	}
--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -8,7 +8,7 @@ import (
 	"firebase.google.com/go/v4/auth"
 )
 
-func AuthMiddleware(authClient *auth.Client, next http.HandlerFunc) http.HandlerFunc {
+func Auth(authClient *auth.Client, next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
@@ -31,4 +31,25 @@ func AuthMiddleware(authClient *auth.Client, next http.HandlerFunc) http.Handler
 		ctx := context.WithValue(r.Context(), "userUID", token.UID)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	}
+}
+
+func Cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		//a origin do frontend nao esta sendo validada ainda
+		origin := r.Header.Get("Origin")
+		if origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
